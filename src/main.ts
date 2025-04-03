d7ee5bde3b46cf7050e3556f313447c5d74936ea
Lionel <lio2014vt@gmail.com>
2025-04-03 15:55:00 -0300

    Updated: Desestructuración de Arreglos: PC-OFICINA

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // import './style.css';
 // import './topics/01-basic-types';
-import './topics/03-functions';
+import './topics/05-basic-destructuring';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
